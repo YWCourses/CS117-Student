1aae5ea5ba50ec82e346766410df9a2910c6fd44
Yeqi <weiyeqi@gmail.com>
2025-11-19 14:29:40 -0700

    wordle game

---
+++ week13/wordle.cpp
@@ -0,0 +1,26 @@
+/* Wordle Game
+ * Author: Yeqi Wei
+ * Date: 2025.11.19
+ * Description: Given a target word of length 5, prompt for user's guess.
+ *    If the user's input is not of 5 characters, prompt for new input.
+ *    Otherwise, compare the user's input with target word.
+ *       Win: if words are matched;
+ *       Loss: otherwise. 
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+   string userInput;
+   string targetWord = "final";
+
+   /* Step 1: get user's input - 5 characters, and store into userInput */
+
+   /* Step 2: compare user's input with the targetWord */
+
+   /* Step 3: Output result - match (win) or unmatch (loss) */
+
+   return 0;
+}
